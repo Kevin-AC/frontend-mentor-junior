51b9b8fd9bfd39b2ab630d79f9ffbafd29c0bda0
Kevin-AC <kevin.argumedo@gmail.com>
2026-01-18 14:43:52 -0500

    feat: implementar modo oscuro en el componente Header y ajustar estilos en index.css

--- github-user-search-app/src/components/Header.jsx
+++ github-user-search-app/src/components/Header.jsx
@@ -1,12 +1,24 @@
+import { useEffect,useState } from "react"
 export default function Header(){
+    const [darkMode,setDarkMode]=useState(false)
+    useEffect(()=>{
+        if(darkMode){
+            document.documentElement.classList.add('dark');
+        }else{
+            document.documentElement.classList.remove('dark')
+        }
+    },[darkMode])
     return (
         <section className="flex justify-between items-center">
             <img src="public/assets/devfinder-light.svg" alt="devfinder-logo" />
-            <div className="flex  gap-4 text-Neutral-200 font-bold tracking-[2.5px] ">
-                <p>LIGHT</p>
-                <img className="w-5 h-5" src="public/assets/icon-sun.png" alt="icon-sun" />
-                <img className="w-5 h-5" src="public/assets/icon-moon.svg" alt="icon-moon.svg" />
-            </div>
+            <button className="flex gap-4 text-Neutral-200 font-bold tracking-[2.5px] cursor-pointer"
+                onClick={() => setDarkMode(!darkMode)}
+            >
+                <p className="hidden dark:block">LIGHT</p>
+                <p className="dark:hidden">DARK</p>
+                <img className="w-5 h-5 hidden dark:block" src="public/assets/icon-sun.png" alt="icon-sun" />
+                <img className="w-5 h-5 dark:hidden" src="public/assets/icon-moon.svg" alt="icon-moon.svg" />
+            </button>
         </section>
     )
 }
