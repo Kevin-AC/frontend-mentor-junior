74d1812ae4d9261171a7d4d482eb8536e8f78bdb
Kevin-AC <kevin.argumedo@gmail.com>
2026-01-18 22:42:19 -0500

    fix(App): ajustar altura mínima del contenedor principal para mejorar la visualización

--- github-user-search-app/src/App.jsx
+++ github-user-search-app/src/App.jsx
@@ -5,7 +5,7 @@ import UserCard from "./components/UserCard"
 function App() {
   const { user,error,getUser } = useFetchUser();
   return (
-    <main className="dark:bg-Neutral-900 bg-Neutral-100 opacity-100 w-full h-screen py-8 px-4 md:py-11 md:px-8 font-space lg:grid lg:place-content-center  ">
+    <main className="dark:bg-Neutral-900 bg-Neutral-100 opacity-100 w-full min-h-screen  py-8 px-4 md:py-11 md:px-8 font-space lg:grid lg:place-content-center  ">
       <div className="w-auto h-auto space-y-8 ">
         <Header/>
         <Search getUser={getUser}/>
