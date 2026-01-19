13e4a1d0a6118c93cd8c64586ab064106fc5de6e
Kevin-AC <kevin.argumedo@gmail.com>
2026-01-18 22:13:18 -0500

    feat: agregar soporte para iconos de modo oscuro y claro, y mejorar estilos de componentes

--- github-user-search-app/src/components/Header.jsx
+++ github-user-search-app/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { useEffect,useState } from "react"
+import { MoonIcon, SunIcon } from "./icons"
+
 export default function Header(){
     const [darkMode,setDarkMode]=useState(false)
     useEffect(()=>{
@@ -12,13 +14,13 @@ export default function Header(){
         <section className="flex justify-between items-center">
             <img className="hidden dark:block" src="/assets/devfinder-light.svg" alt="devfinder-light-logo" />
             <img className="dark:hidden" src="/assets/devfinder.svg" alt="devfinder-logo" />
-            <button className="flex gap-4 text-Neutral-200 font-bold tracking-[2.5px] cursor-pointer"
+            <button className="flex Focus rounded-sm gap-4 text-Neutral-200 hover:text-Neutral-900 dark:hover:text-Neutral-0 font-bold tracking-[2.5px] cursor-pointer"
                 onClick={() => setDarkMode(!darkMode)}
             >
-                <p className="hidden dark:block">LIGHT</p>
-                <p className="dark:hidden">DARK</p>
-                <img className="w-5 h-5 hidden dark:block" src="/assets/icon-sun.png" alt="icon-sun" />
-                <img className="w-5 h-5 dark:hidden" src="/assets/icon-moon.svg" alt="icon-moon.svg" />
+                <p className="hidden dark:block  ">LIGHT</p>
+                <p className="dark:hidden ">DARK</p>
+                <SunIcon className="w-5 h-5 hidden dark:block fill-current" />
+                <MoonIcon className="w-5 h-5 dark:hidden fill-current" />
             </button>
         </section>
     )
