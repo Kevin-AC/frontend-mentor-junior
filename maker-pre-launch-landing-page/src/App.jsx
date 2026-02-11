10311a38eaed192f67cc9654c1dfdb7f13b7ef70
Kevin-AC <kevin.argumedo@gmail.com>
2026-02-11 16:41:29 -0500

    Feat:Main section, MainCard component

--- maker-pre-launch-landing-page/src/App.jsx
+++ maker-pre-launch-landing-page/src/App.jsx
@@ -1,8 +1,33 @@
 import Header from "./components/Header"
+import MainCard from "./components/MainCard"
 function App() {
   return (
     <>
       <Header/>
+      <main className="px-4 pt-20 space-y-20">
+        <section className="space-y-12">
+          <MainCard
+            title={"Indulge your passions"}
+            paragragh={"Your passions shouldn't be just for the weekend. Earn a living doing what you love."}
+            img={"src/assets/illustration-passions.svg"}
+          />
+          <MainCard
+            title={"Gain financial freedom"}
+            paragragh={"Start making money work for you. There’s nothing quite like earning while you sleep. "}
+            img={"src/assets/illustration-financial-freedom.svg"}
+          />
+          <MainCard
+            title={"Choose your lifestyle"}
+            paragragh={"Own your daily schedule. Fancy a lie-in? Go for it! Take charge of your week."}
+            img={"src/assets/illustration-lifestyle.svg"}
+          />
+          <MainCard
+            title={"Work from anywhere"}
+            paragragh={"Selling online means not being pinned down. Want to work AND travel? Go for it!"}
+            img={"src/assets/illustration-work-anywhere.svg"}
+          />
+        </section>
+      </main>
     </>
     
   )
---
+++ maker-pre-launch-landing-page/src/components/MainCard.jsx
@@ -0,0 +1,9 @@
+export default function MainCard({title,paragragh,img}){
+    return(
+        <section className="grid place-items-center">
+            <img src={img} alt="illustration-passions" />
+            <h2 className="Text-Preset-4 text-Neutral-0 pt-8 pb-4">{title}</h2>
+            <p className="text-Blue-Gray-500 text-center">{paragragh}</p>
+        </section>
+    )
+}
