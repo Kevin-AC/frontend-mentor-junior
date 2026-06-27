71e835e22dcf23218b199ace2e617878c0095a79
Kevin-AC <kevin.argumedo@gmail.com>
2026-06-26 20:42:31 -0500

    Fix: correccion tamaño Information Card

--- bmi-calculator/src/components/InformationCard.jsx
+++ bmi-calculator/src/components/InformationCard.jsx
@@ -1,12 +1,12 @@
 export default function InformationCard({ title,result,tip}){
     const hasData = result && result !== "";
     return(
-        <aside className={hasData ? "p-8 bg-Blue-500 text-White rounded-2xl sm:rounded-r-full sm:flex sm:justify-between gap-6" : "p-8 bg-Blue-500 text-White rounded-2xl sm:rounded-r-full"}>
+        <aside className={hasData ? "p-8 bg-Blue-500 text-White rounded-2xl sm:rounded-r-full space-y-6 sm:space-y-0 sm:flex sm:items-center sm:justify-between gap-6" : "p-8 bg-Blue-500 text-White rounded-2xl sm:rounded-r-full"}>
             <div>
                 <h2 className={hasData ? "preset-6-Regular" : "preset-4"}>{title}</h2>
                 {hasData && <p className="preset-1">{result}</p>}
             </div>
-            <p className="sm:max-w-66 lg:max-w-52 preset-7-Regular pt-6">{tip}</p>
+            <p className={hasData ? "sm:w-1/2" :"w-full preset-7-Regular"}>{tip}</p>
         </aside>
     )
 }
